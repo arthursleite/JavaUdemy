b25d1dfab538d885de65bd7a1938e1a52382d289
Arthur Leite <81442283+arthursleite@users.noreply.github.com>
2023-07-12 13:49:23 -0300

    versao2

---
+++ enumeracoesComposicao/AtividadeFixacao/src/entities/Order.java
@@ -0,0 +1,53 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+
+    private Date moment;
+    private OrderStatus status;
+
+    private List OrderItem
+
+    public Order() {
+    }
+
+    public Order(Date moment, OrderStatus status) {
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public List getItems() {
+        return items;
+    }
+
+    public void addItem(OrderItem item) {
+
+    }
+
+    public void removeItem(OrderItem item) {
+
+    }
+
+    public Double total() {
+        return 0.0;
+    }
+}
