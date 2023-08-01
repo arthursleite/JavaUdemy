565e0a1d610d64dcd42311c7e3e45cdc36c82c25
Arthur Leite <81442283+arthursleite@users.noreply.github.com>
2023-08-01 13:46:30 -0300

    intefaces commit1

---
+++ interfaces/Exemplo1/src/model/entities/Vehicle.java
@@ -0,0 +1,23 @@
+//224. Interfaces, 225. Solução do problema - PARTE 1, 226. Solução do problema - PARTE 2 (sem interface
+package model.entities;
+
+public class Vehicle {
+
+    private String model;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+}
